84485e5976ba2c75e0db364ffe2245bf612138cb
Javohir-123 <Javoh.1hacker@gmail.com>
2026-05-05 19:25:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import asyncio
-import logging
 import os
+import logging
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
 
-# TOKEN (Render Environment Variables dan olinadi)
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 
 async def start_handler(message: types.Message):
-    await message.answer("👋 Salom! Men ishlayapman ✅")
+    await message.answer("👋 Salom! Bot ishlayapti ✅")
 
-async
+async def main():
+    if not BOT_TOKEN:
+        print("TOKEN YO‘Q!")
+        return
+
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    dp = Dispatcher()
+
+    dp.message.register(start_handler, Command("start"))
+
+    await bot.delete_webhook()
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
